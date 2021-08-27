d49846b802273326c51558baeada3b0242c1a4ab
lirc572 <lirc572@gmail.com>
2021-08-28 03:21:49 +0800

    fix: remove unused imports

--- src/components/UserOptionList.jsx
+++ src/components/UserOptionList.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { useSelector, useDispatch } from 'react-redux';
-
 import { useHistory } from 'react-router-dom';
 
 import SettingsIcon from '@material-ui/icons/Settings';
